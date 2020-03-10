29b33f68183567ddddd3256d52e3119e44fbcb61
Kristian <krdo@eaaa.dk>
2020-03-10 14:23:59 +0100

    Optimizing MongoDB connection for lambda functions,

--- functions/kittens.js
+++ functions/kittens.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
 const kittenDB = require('./kitten_db')(mongoose);
 
+const url = (process.env.MONGO_URL || 'mongodb://localhost/kitten_db');
+const connect = async () => await mongoose.connect(url, {
+    bufferCommands: false,
+    bufferMaxEntries: 0,
+    useNewUrlParser: true,
+    useUnifiedTopology: true});
+connect().then(_ => console.log("Connected"));
+
 export async function handler(event, context) {
-    const url = (process.env.MONGO_URL || 'mongodb://localhost/kitten_db');
-    await mongoose.connect(url, {
-        bufferCommands: false,
-        bufferMaxEntries: 0,
-        useNewUrlParser: true,
-        useUnifiedTopology: true});
+    context.callbackWaitsForEmptyEventLoop = false;
+
     const kittens = await kittenDB.getKittens();
     console.log("kittens", kittens);
+
     return {
         statusCode: 200,
         body: JSON.stringify(kittens)
