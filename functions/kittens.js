f0bede4c687553c4d02c48f863748f170ca44b6b
Kristian <krdo@eaaa.dk>
2020-03-10 14:03:30 +0100

    Adding mongoose library and mongo connection

--- functions/kittens.js
+++ functions/kittens.js
@@ -1,24 +1,17 @@
-export async function handler(event, context) {
-    const data = [{
-        "hobbies": ["sleeping", "purring", "people watching", "eating"],
-        "_id": "5dd648db675c47c05217a15b",
-        "name": "Tom"
-    }, {
-        "hobbies": ["people watching", "purring", "eating", "sleeping", "asdf"],
-        "_id": "5dd648db675c47c05217a15c",
-        "name": "Garfield"
-    }, {
-        "hobbies": ["purring", "asfd"],
-        "_id": "5dd648db675c47c05217a15d",
-        "name": "Felix",
-    }, {
-        "hobbies": ["sleeping"],
-        "_id": "5dd648db675c47c05217a15e",
-        "name": "Tom"
-    }];
+const mongoose = require('mongoose');
+const kittenDB = require('./kitten_db')(mongoose);
 
+export async function handler(event, context) {
+    const url = (process.env.MONGO_URL || 'mongodb://localhost/kitten_db');
+    await mongoose.connect(url, {
+        bufferCommands: false,
+        bufferMaxEntries: 0,
+        useNewUrlParser: true,
+        useUnifiedTopology: true});
+    const kittens = await kittenDB.getKittens();
+    console.log("kittens", kittens);
     return {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify(kittens)
     };
 }
